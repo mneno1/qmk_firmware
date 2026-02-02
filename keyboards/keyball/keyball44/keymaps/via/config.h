7ffea602a663e3fdd81512e6cbf15859298d51c1
mneno1 <mnenoi.mobile@gmail.com>
2026-02-02 12:31:58 +0900

    Update config.h

--- keyboards/keyball/keyball44/keymaps/via/config.h
+++ keyboards/keyball/keyball44/keymaps/via/config.h
@@ -37,6 +37,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define POINTING_DEVICE_INVERT_X
+#define POINTING_DEVICE_INVERT_Y
 #define AUTO_MOUSE_DEFAULT_LAYER 5
 #define AUTO_MOUSE_TIME 1000
 #define SCROLL_MODE_LAYER 7
